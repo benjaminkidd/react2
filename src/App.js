2c888a970c9a5618c7bc653dac0dc1996e5a840d
Benjamin Kidd <benkidd361@gmail.com>
2020-08-02 15:39:13 -0400

    added person class

--- src/App.js
+++ src/App.js
@@ -6,12 +6,11 @@ import Person from './Person/Person';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Person />
-        <Person />
-        <Person />
-        <Person />
-      </div>
+        <div className="App">
+          <Person age="24" name="Bill"/>
+          <Person age="24" name="Ted"/>
+          <Person age="24" name="Neo">hobbies: guitar</Person>
+        </div>
     );
   }
 }
